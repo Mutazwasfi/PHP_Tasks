bfdedf2690bd720a328317d125ac658e8cfaff3c
“mutazwasfi” <mutazwasfi5@gmail.com>
2023-08-03 18:46:28 +0300

    WiP

--- Task 2-8/Tasks Logical statments.php
+++ Task 2-8/Tasks Logical statments.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task 2-8</title>
+    <title>Logical operators</title>
 </head>
 <body>
     <?php
---
+++ Task 2-8/Tasks Loops.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops</title>
+</head>
+<body>
+    <?php
+        echo "Task 1<br><br>";
+        $year1=2013;
+
+        if ($year1%4==0) {
+            echo $year1. " is a leap year ";
+        }
+        else {
+            echo $year1. " is not a leap year ";
+        }
+        
+    ?>
+</body>
+</html>
