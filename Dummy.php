93e4481ccc2d2c11bc170178c950a53c4bfbbb91
Mutazwasfi <Mutazwasfi5@gmail.com>
2023-08-06 15:37:37 +0300

    work

--- Dummy.php
+++ Dummy.php
@@ -4,58 +4,66 @@
 
 <?php
 
-echo "------------------ get_declared_classes : -------------------  <br>";
+echo "------------------ var_dump : -------------------  <br>";
 
 echo '<br>';
 
-print_r(get_declared_classes());
+$a = 32;
+echo var_dump($a) . "<br>";
+
+$b = "Hello world!";
+echo var_dump($b) . "<br>";
+
+$c = 32.5;
+echo var_dump($c) . "<br>";
+
+$d = array("red", "green", "blue");
+echo var_dump($d) . "<br>";
+
+$e = array(32, "Hello world!", 32.5, array("red", "green", "blue"));
+echo var_dump($e) . "<br>";
+
+// Dump two variables
+echo var_dump($a, $b) . "<br>";
 
 
-echo '<br><br>';
-
-
-echo "------------------ get_class : -------------------  <br>";
-
-
-class foo {
-    function name()
-    {
-        echo "My name is " , get_class($this) , "<br>";
-    }
-}
-
-// create an object
-$bar = new foo();
-
-// external call
-echo "Its name is " , get_class($bar) , "<br>";
-
-// internal call
-$bar->name();
-
-
-echo "------------------ get_class_vars : -------------------  <br>";
 
 echo '<br>';
 
-class myclass {
 
-    public $var1; // this has no default value...
-    public $var2 = "xyz";
-    public $var3 = 100;
-    private $var4;
-
-}
-
-$my_class = new myclass();
-
-$class_vars = get_class_vars(get_class($my_class));
-
-foreach ($class_vars as $name => $value) {
-    echo "$name : $value<br>";
-}
+echo "------------------ var_export : -------------------  <br>";
 
 echo '<br>';
+
+$a = 32;
+echo var_export($a) . "<br>";
+
+$b = "Hello world!";
+echo var_export($b) . "<br>";
+
+$c = 32.5;
+echo var_export($c) . "<br>";
+
+$d = array("red", "green", "blue");
+echo var_export($d) . "<br>";
+
+$e = array(32, "Hello world!", 32.5, array("red", "green", "blue"));
+echo var_export($e) . "<br>";
+
+echo '<br>';
+
+echo "------------------ print_r : -------------------  <br>";
+
+echo '<br>';
+
+$a = array("red", "green", "blue");
+print_r($a);
+
+echo "<br>";
+
+$b = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+print_r($b);
+
 echo '<br>';
 
 
