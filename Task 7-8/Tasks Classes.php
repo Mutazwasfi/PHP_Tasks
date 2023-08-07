5dcd1f1330686e245461067110164ccd71212ef2
“mutazwasfi” <mutazwasfi5@gmail.com>
2023-08-07 23:50:44 +0300

    WiP

---
+++ Task 7-8/Tasks Classes.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+</head>
+<body>
+  <?php
+
+  echo "Task 1<br><br>";
+
+  class outputMsg{
+    public function __construct() {
+   echo "MyClass class has initialized !";
+  }
+ }
+  $display = new outputMsg;
+
+  echo "<hr>";
+  echo "Task 2<br><br>";
+  
+  class greeting {
+    public $msg2 ="Hello All, I am ";
+
+    function greet($name) {
+       return " $this->msg2.$name";
+    }
+ }
+
+  $newPerson = new greeting();
+  echo $newPerson->greet("scott");
+
+  
+  echo "<hr>";
+  echo "Task 3<br><br>";
+
+  class factorial_of_a_number {
+  protected $_n;
+  public function __construct($n)
+   {
+     if (!is_int($n))
+	   {
+	      throw new InvalidArgumentException('Not a number or missing argument');
+      }
+    $this->_n = $n;
+	}
+  public function result()
+    {
+     $factorial = 1;
+     for ($i = 1; $i <= $this->_n; $i++)
+	  {
+	    $factorial *= $i;
+    }
+	   return $factorial;
+	 }
+ }
+
+  $newfactorial = New factorial_of_a_number(6);
+  echo $newfactorial->result();
+
+  
+  echo "<hr>";
+  echo "Task 4<br><br>";
+
+  class array_sort {
+    protected $_asort;
+    
+    public function __construct(array $asort)
+     {
+        $this->_asort = $asort;
+     }
+    public function sortedarr()
+     {
+        $sorted = $this->_asort;
+        sort($sorted);
+        return $sorted;
+      }
+  }
+  $sortarray = new array_sort(array(11, -2, 4, 35, 0, 8, -9));
+  print_r($sortarray->sortedarr());
+
+
+  ?>
+</body>
+</html>
---
+++ Task 7-8/Tasks Super Global Variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Global Variables</title>
+</head>
+<body>
+  <?php
+
+  echo "Task 1<br><br>";
+
+  
+
+
+
+  echo "<hr>";
+  echo "Task 4<br><br>";
+
+  ?>
+</body>
+</html>
