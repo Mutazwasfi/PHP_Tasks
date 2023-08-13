655965c9d08721642697353936209bcb377e9341
“mutazwasfi” <mutazwasfi5@gmail.com>
2023-08-13 16:57:04 +0300

    WiP

---
+++ phpMiniProject/signUp.js
@@ -0,0 +1,38 @@
+
+welcome = document.querySelector('.welcome');
+signUp = document.querySelector('.sign-up');
+signIn = document.querySelector('.sign-in');
+
+
+btn1 = document.querySelector('.wlcBtnSignUP');
+btn2 = document.querySelector('.wlcBtnLogIn');
+btn3 = document.querySelector('.opposite-btn1');
+btn4 = document.querySelector('.opposite-btn2');
+
+
+// Switches to 'Sign Up'
+btn1.addEventListener('click', () =>{
+  welcome.style.display = 'none';
+  signUp.style.display = 'block';
+  signIn.style.display = 'none';  
+});
+
+btn3.addEventListener('click', () => {
+  welcome.style.display = 'none'; 
+  signUp.style.display = 'block';
+  signIn.style.display = 'none'; 
+});
+
+// Switches to 'Sign In'
+btn2.addEventListener('click', () =>{
+  welcome.style.display = 'none'; 
+  signUp.style.display = 'none';
+  signIn.style.display = 'block';
+});
+
+btn4.addEventListener('click', () => {
+  welcome.style.display = 'none';
+  signUp.style.display = 'none';
+  signIn.style.display = 'block';
+});
+
